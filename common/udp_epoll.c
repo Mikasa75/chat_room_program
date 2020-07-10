b876ebe564241cd68369f75deb47a0e0c4ee505c
Furysaving <1254336210@qq.com>
2020-07-11 00:36:17 +0800

    0036

--- common/udp_epoll.c
+++ common/udp_epoll.c
@@ -128,7 +128,7 @@ int udp_accept(int fd, struct User* user) {
     bzero(&r_msg,sizeof(r_msg));
 
 
-        sprintf(r_msg.msg,"User"RED" %s"NONE"上线了!",user->name);
+        sprintf(r_msg.msg,"User %s 上线了!",user->name);
         printf("User"RED" %s"NONE"上线了!",user->name);
         strcpy(r_msg.name,user->name);)
         r_msg.type = CHAT_SYS;
